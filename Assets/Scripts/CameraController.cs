c605dc71cb177a5c28663a48a9c7643560d9065c
Jacudibu <jacudibu@gmail.com>
2019-11-21 21:25:49 +0100

    Started playing around with controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private float rotationLerpSpeed = 2f;
+    [SerializeField] private float positionLerpSpeed = 5f;
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            targetTransform.position,
+            Time.deltaTime * positionLerpSpeed);
+        
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation, 
+            targetTransform.rotation, 
+            Time.deltaTime * rotationLerpSpeed);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 _screenSize;
+    public float rotationSpeed;
+
+    public bool invertX = false;
+    public bool invertY = true;
+    
+    void Start()
+    {
+        _screenSize = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
+    }
+
+    void Update()
+    {
+        var cursorPos = Input.mousePosition;
+
+        var relativePos = cursorPos - _screenSize;
+        var clampedPos = Vector2.ClampMagnitude(relativePos, 1000);
+        
+        var rotation = new Vector3(
+            clampedPos.y * (invertX ? 1 : -1),
+            clampedPos.x * (invertY ? -1 : 1),
+            0);
+        rotation *= rotationSpeed;
+        rotation *= Time.deltaTime;
+        
+        transform.Rotate(rotation);
+    }
+}
