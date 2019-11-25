1f290d1313c72d8fd7716821a107d34535fab938
Jacudibu <jacudibu@gmail.com>
2019-11-25 22:09:41 +0100

    Strafing now moves the player ship a little

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,72 +8,55 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform shipTransform;
+    [SerializeField] private Transform cameraAnchor;
     
     private Vector3 _screenSize;
+    private Vector3 _defaultLocalCameraAnchorPosition;
     public float rotationSpeed = 0.4f;
     public float shipAlignSpeed = 4;
 
     public bool invertX = false;
     public bool invertY = true;
 
-    private int controlCircleRadius = 200;
-
+    public int controlCircleRadius = 200;
+    
     public float acceleration = 100;
     public float maxSpeed = 500;
     public float minSpeed = 300;
     public float currentSpeed;
 
-    public float strafeSpeed = 5;
+    public float strafeAnimationSpeed = 5;
     public float strafeValue;
+    public float strafeTravelSpeed = 10;
+    
+    private bool _strafeLeft;
+    private bool _strafeRight;
     
     void Start()
     {
         _screenSize = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
+        _defaultLocalCameraAnchorPosition = cameraAnchor.transform.localPosition;
     }
 
     void Update()
     {
+        _strafeLeft = Input.GetKey(KeyBindings.StrafeLeft);
+        _strafeRight = Input.GetKey(KeyBindings.StrafeRight);
+        
         Strafe();
         RotateShip();
         CalculateSpeed();
         MoveShip();
+        MoveCameraAnchor();
     }
 
-    private void Strafe()
+    private void MoveCameraAnchor()
     {
-        var strafeLeft = Input.GetKey(KeyBindings.StrafeLeft);
-        var strafeRight = Input.GetKey(KeyBindings.StrafeRight);
-
-        if ((!strafeLeft && !strafeRight) || (strafeLeft && strafeRight))
-        {
-            if (strafeValue > 0.01f)
-            {
-                strafeValue -= strafeSpeed * Time.deltaTime;
-            } 
-            else if (strafeValue < -0.01f)
-            {
-                strafeValue += strafeSpeed * Time.deltaTime;
-            }
-            else
-            {
-                strafeValue = 0f;
-            }
-        }
-        else
-        {
-            if (strafeLeft)
-            {
-                strafeValue += strafeSpeed * Time.deltaTime;
-            } 
-            else ;
-            {
-                strafeValue -= strafeSpeed * Time.deltaTime;
-            }
-
-            strafeValue = Mathf.Clamp(strafeValue, -1f, 1f);
-        }
+        cameraAnchor.transform.localPosition = _defaultLocalCameraAnchorPosition + new Vector3(
+                                                   strafeValue * 2,
+                                                   0, 0);
     }
-    
+
     private void RotateShip()
     {
         var cursorPos = Input.mousePosition;
@@ -95,14 +78,47 @@ public class PlayerController : MonoBehaviour
         );
 
         var targetShipRotation = new Vector3(
-            clampedPercentage.y * 30 * (invertX ? 1 : -1),
-            0, 
-            clampedPercentage.x * 30 * (invertY ? -1 : 1) * (clampedPercentage.y > 0 ? -1 : 0));
+            clampedPercentage.y * 20 * (invertX ? 1 : -1),
+            0,
+            strafeRotation.z);  //clampedPercentage.x * 30 * (invertY ? -1 : 1) * (clampedPercentage.y > 0 ? -1 : 0));
 
         shipTransform.localRotation = Quaternion.Lerp(
             shipTransform.localRotation, 
-            Quaternion.Euler(strafeRotation + targetShipRotation),
+            Quaternion.Euler(targetShipRotation),
             Time.deltaTime * shipAlignSpeed);
+            
+    }
+
+    private void Strafe()
+    {
+        if ((!_strafeLeft && !_strafeRight) || (_strafeLeft && _strafeRight))
+        {
+            if (strafeValue > 0.01f)
+            {
+                strafeValue -= strafeAnimationSpeed * Time.deltaTime;
+            } 
+            else if (strafeValue < -0.01f)
+            {
+                strafeValue += strafeAnimationSpeed * Time.deltaTime;
+            }
+            else
+            {
+                strafeValue = 0f;
+            }
+        }
+        else
+        {
+            if (_strafeLeft)
+            {
+                strafeValue += strafeAnimationSpeed * Time.deltaTime;
+            } 
+            else
+            {
+                strafeValue -= strafeAnimationSpeed * Time.deltaTime;
+            }
+
+            strafeValue = Mathf.Clamp(strafeValue, -1f, 1f);
+        }
     }
 
     private void CalculateSpeed()
@@ -130,6 +146,17 @@ public class PlayerController : MonoBehaviour
 
     private void MoveShip()
     {
-        transform.Translate(Time.deltaTime * currentSpeed * Vector3.forward);
+        var movement = currentSpeed * Vector3.forward;
+
+        if (_strafeLeft)
+        {
+            movement.x -= strafeTravelSpeed;
+        }
+        if (_strafeRight)
+        {
+            movement.x += strafeTravelSpeed;
+        }
+        
+        transform.Translate(Time.deltaTime * movement);
     }
 }
