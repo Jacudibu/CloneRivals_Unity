476c8d6cd1363e1894bfa5c0d12549d3ddd86449
Jacudibu <jacudibu@gmail.com>
2019-11-28 18:29:56 +0100

    Added Missiles.

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionEffect;
+    
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float speed = 50;
+    [SerializeField] private float rotationDegrees = 110;
+    
+    void Update()
+    {
+        if (target != null)
+        {
+            var targetRotation = Quaternion.LookRotation(target.position - transform.position);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * rotationDegrees);
+        }
+        
+        transform.Translate(Time.deltaTime * speed * Vector3.forward);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent != null)
+        {
+            if (other.transform.parent.parent != null)
+            {
+                if (other.transform.parent.parent.parent != null)
+                {
+                    if (other.transform.parent.parent.parent.GetComponent<PlayerController>())
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject, 1f);
+
+        transform.GetChild(0).gameObject.SetActive(false);
+        enabled = false;
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -8,19 +8,24 @@ public class WeaponController : MonoBehaviour
     public float SecondaryReattackTime = 1f;
     private float _lastSecondaryAttackTime;
     
+    [SerializeField] private GameObject missilePrefab;
+    [SerializeField] private Transform[] missileSpawnPoints;
+    [SerializeField] private Transform target;
     
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
         if (KeyBindings.IsSecondary())
         {
-            if (Time.time - _lastSecondaryAttackTime < SecondaryReattackTime)
+            if (Time.time - _lastSecondaryAttackTime > SecondaryReattackTime)
             {
-                // TODO: Pew pew
+                foreach (var missileSpawnPoint in missileSpawnPoints)
+                {
+                    var obj = Instantiate(missilePrefab, missileSpawnPoint.position, missileSpawnPoint.rotation);
+                    var missile = obj.GetComponent<Missile>();
+                    missile.SetTarget(target);
+                }
+
+                _lastSecondaryAttackTime = Time.time;
             }
         }
     }
