617a7f519d66fce9902f3e95c7ef3ae8bc9ef111
Jacudibu <jacudibu@gmail.com>
2019-11-28 14:53:33 +0100

    Started working on a weapon controller thing

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using InputConfiguration;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public float SecondaryReattackTime = 1f;
+    private float _lastSecondaryAttackTime;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyBindings.SecondaryWeapon))
+        {
+            if (Time.time - _lastSecondaryAttackTime < SecondaryReattackTime)
+            {
+                // TODO: Pew pew
+            }
+        }
+    }
+}
