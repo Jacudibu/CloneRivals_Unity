1c6d1744234a53984c96b574067b8b15c005b0de
Jacudibu <jacudibu@gmail.com>
2019-12-01 16:27:12 +0100

    Added TargetManager (and the MouseCursor script which I totally didn't forget to add)

---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    [SerializeField] private float cursorRotationOffset = -90;
+    private Transform _transform;
+    private Vector3 _screenSize;
+    
+    
+    private void Start()
+    {
+        _transform = transform;
+        OnEnable();
+    }
+    
+    private void OnEnable()
+    {
+        _screenSize = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
+        Cursor.visible = false;
+    }
+
+    private void OnDisable()
+    {
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        var mousePosition = Input.mousePosition - _screenSize;
+        
+        var angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        _transform.rotation = Quaternion.Euler(0, 0, angle + cursorRotationOffset);
+        _transform.position = Input.mousePosition;
+    }
+}
---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public List<GameObject> thingsTargetingMe;
+    public GameObject target;
+
+    public GameObject targetArrow;
+    public GameObject targetMeArrow;
+    private List<GameObject> _thingsTargetingMeArrows;
+    
+    public float radius = 100;
+    public float arrowRotationOffset = -90;
+
+    private void Start()
+    {
+        _thingsTargetingMeArrows = new List<GameObject>()
+        {
+            targetMeArrow
+        };
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            AdjustArrowTransform(targetArrow.transform, target.transform.position);
+        }
+
+        if (_thingsTargetingMeArrows.Count < thingsTargetingMe.Count)
+        {
+            var original = _thingsTargetingMeArrows[0];
+
+            while (_thingsTargetingMeArrows.Count < thingsTargetingMe.Count)
+            {
+                var arrow = Instantiate(original, original.transform.parent, true);
+                _thingsTargetingMeArrows.Add(arrow);
+            }
+        }
+
+        for (var i = 0; i < thingsTargetingMe.Count; i++)
+        {
+            AdjustArrowTransform(_thingsTargetingMeArrows[i].transform, thingsTargetingMe[i].transform.position);
+        }
+    }
+
+    private void AdjustArrowTransform(Transform arrowTransform, Vector3 position)
+    {
+        var relativePos = transform.InverseTransformPoint(position);
+        var normalizedRelativePos = Vector3.Normalize(new Vector3(relativePos.x, relativePos.y, 0));
+        arrowTransform.position = normalizedRelativePos * radius + new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+            
+        var angle = Mathf.Atan2(normalizedRelativePos.y, normalizedRelativePos.x) * Mathf.Rad2Deg;
+        arrowTransform.rotation = Quaternion.Euler(0, 0, angle + arrowRotationOffset);
+    }
+
+}
