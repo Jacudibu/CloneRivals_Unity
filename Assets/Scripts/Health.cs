847f110818d58fb10603c47e9eef51040c4b5a4a
Jacudibu <jacudibu@gmail.com>
2019-12-02 14:54:30 +0100

    Added a health script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int currentHealth;
+    [SerializeField] private int maxHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public int AddHealth(int amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        return currentHealth;
+    }
+
+    public int SubtractHealth(int amount)
+    {
+        currentHealth -= amount;
+        return currentHealth;
+    }
+}
