897aa9236490c7fe508850bdd93dff6a97e118f5
Jacudibu <jacudibu@gmail.com>
2019-12-03 10:12:00 +0100

    Reticle now only appears when target is within a certain angle

--- Assets/Scripts/TargetManager.cs
+++ Assets/Scripts/TargetManager.cs
@@ -6,6 +6,7 @@ public class TargetManager : MonoBehaviour
     public List<GameObject> thingsTargetingMe;
     public GameObject target;
     public GameObject lockOnReticle;
+    public float lockOnAngle = 6;
     
     public GameObject targetArrow;
     public GameObject targetMeArrow;
@@ -15,10 +16,12 @@ public class TargetManager : MonoBehaviour
     public float arrowRotationOffset = -90;
     private Camera _camera;
     private Plane[] _cameraFrustumPlanes;
+    private Transform _shipTransform;
     
     private void Start()
     {
         _camera = Camera.main;
+        _shipTransform = GetComponentInChildren<MeshRenderer>().transform;
         _thingsTargetingMeArrows = new List<GameObject>
         {
             targetMeArrow
@@ -42,8 +45,16 @@ public class TargetManager : MonoBehaviour
             else
             {
                 targetArrow.SetActive(false);
-                lockOnReticle.SetActive(true);
-                lockOnReticle.transform.position = _camera.WorldToScreenPoint(target.transform.position);
+
+                var angle = Vector3.Angle(_shipTransform.forward, target.transform.position - _shipTransform.position);
+                var lockable = angle > -lockOnAngle && angle < lockOnAngle;
+                
+                lockOnReticle.SetActive(lockable);
+
+                if (lockable)
+                {
+                    lockOnReticle.transform.position = _camera.WorldToScreenPoint(target.transform.position);
+                }
             }
         }
 
