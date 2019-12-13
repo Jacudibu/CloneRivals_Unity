ca24bc8859823b9f89e9daf51b48430ab6a8354d
Jacudibu <jacudibu@gmail.com>
2019-12-13 10:51:46 +0100

    SkillElements now have a text to show remaining cooldown time.

--- Assets/Scripts/UI/SkillHotbarElement.cs
+++ Assets/Scripts/UI/SkillHotbarElement.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,17 +10,18 @@ namespace UI
     {
         [SerializeField] private Image cooldownFillerImage;
         [SerializeField] private Image icon;
-        private float _lastTimeUsed = 0f;
+        [SerializeField] private TextMeshProUGUI cooldownText;
+        
         private float _cooldown = 0f;
 
         private void Start()
         {
             cooldownFillerImage.fillAmount = 0;
+            cooldownText.enabled = false;
         }
         
         public void OnSkillUsed(float cooldown)
         {
-            _lastTimeUsed = Time.time;
             _cooldown = cooldown;
 
             if (Math.Abs(_cooldown) < 0.01)
@@ -32,6 +34,9 @@ namespace UI
 
         private IEnumerator CooldownCoroutine()
         {
+            cooldownFillerImage.fillAmount = 0f;
+            cooldownText.text = _cooldown.ToString("F0");
+            cooldownText.enabled = true;
             yield return new WaitForEndOfFrame();
             
             var elapsedTime = Time.deltaTime;
@@ -39,11 +44,26 @@ namespace UI
             {
                 cooldownFillerImage.fillAmount = 1f - elapsedTime / _cooldown;
                 elapsedTime += Time.deltaTime;
-                
+
+                var remainingTime = _cooldown - elapsedTime;
+
+                if (remainingTime > 60)
+                {
+                    cooldownText.text = $"{remainingTime / 60:F0}:{Mathf.CeilToInt(remainingTime%60):D2}";
+                }
+                else if (remainingTime >= 1f)
+                {
+                    cooldownText.text = Mathf.Ceil(remainingTime).ToString("F0");
+                }
+                else
+                {
+                    cooldownText.text = remainingTime.ToString("F2");
+                }
                 yield return new WaitForEndOfFrame();
             }
 
             cooldownFillerImage.fillAmount = 0f;
+            cooldownText.enabled = false;
         }
         
     }
