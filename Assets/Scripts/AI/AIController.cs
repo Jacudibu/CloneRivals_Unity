e227c6c432cf44dc079251cbf50b6cdfb9caf09e
Jacudibu <jacudibu@gmail.com>
2019-12-19 16:33:51 +0100

    AI Controlled ship now can shoot at player

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -1,22 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace AI
 {
     [RequireComponent(typeof(WeaponController))]
+    [RequireComponent(typeof(TargetManager))]
     [RequireComponent(typeof(Engine))]
     public class AIController : MonoBehaviour
     {
         private Engine _engine;
         private WeaponController _weaponController;
+        private TargetManager _targetManager;
     
         void Start()
         {
             _engine = GetComponent<Engine>();
             _weaponController = GetComponent<WeaponController>();
+            _targetManager = GetComponent<TargetManager>();
         }
 
         void Update()
         {
+            if (_targetManager.Target == null || _targetManager.Target.GetComponent<PlayerController>() == null)
+            {
+                _targetManager.SearchForTarget();
+            }
+
             if (_weaponController.IsMissileLockable)
             {
                 _weaponController.FireMissile();
