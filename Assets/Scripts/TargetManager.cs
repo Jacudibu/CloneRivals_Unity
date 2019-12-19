a41e10fca2cc9be73eb54d93c4820862296eade6
Jacudibu <jacudibu@gmail.com>
2019-12-19 14:13:59 +0100

    Extracted UI Stuff from TargetManager.cs

--- Assets/Scripts/TargetManager.cs
+++ Assets/Scripts/TargetManager.cs
@@ -8,18 +8,8 @@ public class TargetManager : MonoBehaviour
 {
     public List<GameObject> thingsTargetingMe;
     public GameObject Target { get; private set; }
-    public GameObject currentTargetIndicator;
-    public GameObject lockOnReticle;
 
-    public GameObject targetArrow;
-    public GameObject targetMeArrow;
-    private List<GameObject> _thingsTargetingMeArrows;
-
-    public float radius = 100;
-    public float arrowRotationOffset = -90;
-    private Camera _camera;
     private Plane[] _cameraFrustumPlanes;
-    private WeaponController _weaponController;
 
     private Transform _shipTransform;
     [SerializeField] private float targetLockRange = 200f;
@@ -28,17 +18,12 @@ public class TargetManager : MonoBehaviour
     public readonly EventHub.TargetableObjectEvent OnTargetUnLock = new EventHub.TargetableObjectEvent();
     public readonly EventHub.TargetableObjectEvent OnTargetLock = new EventHub.TargetableObjectEvent();
     
+    public delegate void OnTargetingMeDelegate();
+    public event OnTargetingMeDelegate OnIncomingTargetUpdate;
+    
     private void Start()
     {
-        _camera = Camera.main;
-        _weaponController = GetComponent<WeaponController>();
         _shipTransform = GetComponentInChildren<MeshRenderer>().transform;
-        _thingsTargetingMeArrows = new List<GameObject>
-        {
-            targetMeArrow
-        };
-        
-        InitializeOrDisableArrows();
 
         EventHub.OnTargetableDestroyed.AddListener(OnTargetableDestroyed);
     }
@@ -52,7 +37,7 @@ public class TargetManager : MonoBehaviour
         }
         
         thingsTargetingMe.Remove(targetable.gameObject);
-        InitializeOrDisableArrows();
+        OnIncomingTargetUpdate?.Invoke();
     }
 
     private void Update()
@@ -83,98 +68,4 @@ public class TargetManager : MonoBehaviour
             OnTargetLock.Invoke(Target.GetComponent<TargetableObject>());
         }
     }
-
-    private void LateUpdate()
-    {
-        _cameraFrustumPlanes = GeometryUtility.CalculateFrustumPlanes(_camera);
-
-        if (Target != null)
-        {
-            if (!IsObjectInScreenArea(Target.transform.position))
-            {
-                targetArrow.SetActive(true);
-                lockOnReticle.SetActive(false);
-                currentTargetIndicator.SetActive(false);
-                AdjustArrowTransform(targetArrow.transform, Target.transform.position);
-            }
-            else
-            {
-                targetArrow.SetActive(false);
-                currentTargetIndicator.SetActive(true);
-
-                var targetScreenPosition = _camera.WorldToScreenPoint(Target.transform.position);
-                currentTargetIndicator.transform.position = targetScreenPosition;
-                
-                if (_weaponController.IsMissileLockable)
-                {
-                    lockOnReticle.SetActive(true);
-                    lockOnReticle.transform.position = targetScreenPosition;
-                }
-                else
-                {
-                    lockOnReticle.SetActive(false);
-                }
-            }
-        }
-        else
-        {
-            targetArrow.SetActive(false);
-            lockOnReticle.SetActive(false);
-            currentTargetIndicator.SetActive(false);
-        }
-
-        for (var i = 0; i < thingsTargetingMe.Count; i++)
-        {
-            var thing = thingsTargetingMe[i];
-            var arrow = _thingsTargetingMeArrows[i];
-
-            if (!IsObjectInScreenArea(thing.transform.position))
-            {
-                arrow.SetActive(true);
-                AdjustArrowTransform(arrow.transform, thing.transform.position);
-            }
-            else
-            {
-                arrow.SetActive(false);
-            }
-        }
-    }
-
-    private void InitializeOrDisableArrows()
-    {
-        if (_thingsTargetingMeArrows.Count < thingsTargetingMe.Count)
-        {
-            var original = _thingsTargetingMeArrows[0];
-
-            while (_thingsTargetingMeArrows.Count < thingsTargetingMe.Count)
-            {
-                var arrow = Instantiate(original, original.transform.parent, true);
-                _thingsTargetingMeArrows.Add(arrow);
-            }
-        } 
-        else if (_thingsTargetingMeArrows.Count > thingsTargetingMe.Count)
-        {
-            for (var i = thingsTargetingMe.Count; i < _thingsTargetingMeArrows.Count; i++)
-            {
-                _thingsTargetingMeArrows[i].SetActive(false);
-            }
-        }
-    }
-    
-    private bool IsObjectInScreenArea(Vector3 position)
-    {
-        var bounds = new Bounds(position, Vector3.one);
-        return GeometryUtility.TestPlanesAABB(_cameraFrustumPlanes, bounds);
-    }
-    
-    private void AdjustArrowTransform(Transform arrowTransform, Vector3 position)
-    {
-        var relativePos = transform.InverseTransformPoint(position);
-        var normalizedRelativePos = Vector3.Normalize(new Vector3(relativePos.x, relativePos.y, 0));
-        arrowTransform.position = normalizedRelativePos * radius + new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
-            
-        var angle = Mathf.Atan2(normalizedRelativePos.y, normalizedRelativePos.x) * Mathf.Rad2Deg;
-        arrowTransform.rotation = Quaternion.Euler(0, 0, angle + arrowRotationOffset);
-    }
-
 }
---
+++ Assets/Scripts/UI/TargetingUI.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class TargetingUI : MonoBehaviour
+    {
+        private TargetManager _targetManager;
+        private WeaponController _weaponController;
+    
+        public GameObject currentTargetIndicator;
+        public GameObject lockOnReticle;
+    
+        public GameObject targetArrow;
+        public GameObject targetMeArrow;
+        private List<GameObject> _thingsTargetingMeArrows;
+    
+        public float radius = 200;
+        public float arrowRotationOffset = -90;
+        private Camera _camera;
+        private Plane[] _cameraFrustumPlanes;
+    
+        void Start()
+        {
+            var player = FindObjectOfType<PlayerController>();
+            _targetManager = player.GetComponent<TargetManager>();
+            _weaponController = player.GetComponent<WeaponController>();
+        
+            _camera = Camera.main;
+            _thingsTargetingMeArrows = new List<GameObject>
+            {
+                targetMeArrow
+            };
+
+            InitializeOrDisableArrows();
+            _targetManager.OnIncomingTargetUpdate += InitializeOrDisableArrows;
+        }
+
+        private void InitializeOrDisableArrows()
+        {
+            if (_thingsTargetingMeArrows.Count < _targetManager.thingsTargetingMe.Count)
+            {
+                var original = _thingsTargetingMeArrows[0];
+
+                while (_thingsTargetingMeArrows.Count < _targetManager.thingsTargetingMe.Count)
+                {
+                    var arrow = Instantiate(original, original.transform.parent, true);
+                    _thingsTargetingMeArrows.Add(arrow);
+                }
+            } 
+            else if (_thingsTargetingMeArrows.Count > _targetManager.thingsTargetingMe.Count)
+            {
+                for (var i = _targetManager.thingsTargetingMe.Count; i < _thingsTargetingMeArrows.Count; i++)
+                {
+                    _thingsTargetingMeArrows[i].SetActive(false);
+                }
+            }
+        }
+
+        private void LateUpdate()
+        {
+            _cameraFrustumPlanes = GeometryUtility.CalculateFrustumPlanes(_camera);
+
+            var target = _targetManager.Target;
+            if (target != null)
+            {
+                if (!IsObjectInScreenArea(target.transform.position))
+                {
+                    targetArrow.SetActive(true);
+                    lockOnReticle.SetActive(false);
+                    currentTargetIndicator.SetActive(false);
+                    AdjustArrowTransform(targetArrow.transform, target.transform.position);
+                }
+                else
+                {
+                    targetArrow.SetActive(false);
+                    currentTargetIndicator.SetActive(true);
+
+                    var targetScreenPosition = _camera.WorldToScreenPoint(target.transform.position);
+                    currentTargetIndicator.transform.position = targetScreenPosition;
+                
+                    if (_weaponController.IsMissileLockable)
+                    {
+                        lockOnReticle.SetActive(true);
+                        lockOnReticle.transform.position = targetScreenPosition;
+                    }
+                    else
+                    {
+                        lockOnReticle.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                targetArrow.SetActive(false);
+                lockOnReticle.SetActive(false);
+                currentTargetIndicator.SetActive(false);
+            }
+
+            for (var i = 0; i < _targetManager.thingsTargetingMe.Count; i++)
+            {
+                var thing = _targetManager.thingsTargetingMe[i];
+                var arrow = _thingsTargetingMeArrows[i];
+
+                if (!IsObjectInScreenArea(thing.transform.position))
+                {
+                    arrow.SetActive(true);
+                    AdjustArrowTransform(arrow.transform, thing.transform.position);
+                }
+                else
+                {
+                    arrow.SetActive(false);
+                }
+            }
+        }
+
+        private bool IsObjectInScreenArea(Vector3 position)
+        {
+            var bounds = new Bounds(position, Vector3.one);
+            return GeometryUtility.TestPlanesAABB(_cameraFrustumPlanes, bounds);
+        }
+
+        private void AdjustArrowTransform(Transform arrowTransform, Vector3 position)
+        {
+            var relativePos = _targetManager.transform.InverseTransformPoint(position);
+            var normalizedRelativePos = Vector3.Normalize(new Vector3(relativePos.x, relativePos.y, 0));
+            arrowTransform.position = normalizedRelativePos * radius + new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+            
+            var angle = Mathf.Atan2(normalizedRelativePos.y, normalizedRelativePos.x) * Mathf.Rad2Deg;
+            arrowTransform.rotation = Quaternion.Euler(0, 0, angle + arrowRotationOffset);
+        }
+    }
+}
