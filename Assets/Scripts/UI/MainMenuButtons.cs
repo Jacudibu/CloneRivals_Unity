80455552ce7152f0861a184156386e737bf4e05c
Jacudibu <jacudibu@gmail.com>
2019-12-23 17:48:15 +0100

    Added a Main Menu

---
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void PlayOffline()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+}
