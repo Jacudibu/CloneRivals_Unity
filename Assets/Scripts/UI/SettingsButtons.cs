5ac7ad77a877bc3ee7df87a52b1e9d63efacc5cc
Jacudibu <jacudibu@gmail.com>
2019-12-25 16:14:09 +0100

    Enhanced Settings UI

--- Assets/Scripts/UI/SettingsButtons.cs
+++ Assets/Scripts/UI/SettingsButtons.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,9 +7,19 @@ namespace UI
 {
     public class SettingsButtons : MonoBehaviour
     {
+        [SerializeField] private GameObject tabParent;
+
         public void SaveAndExit()
         {
             SceneManager.LoadScene("MainMenu");
+        }
+
+        public void OpenTab(string tabName)
+        {
+            foreach (Transform child in tabParent.transform)
+            {
+                child.gameObject.SetActive(child.name.Equals(tabName));
+            }
         }
     }
 }
