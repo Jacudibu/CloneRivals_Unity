7797b4aafcf432f4ae8b61151ee5730e1e57f2e5
Jacudibu <jacudibu@gmail.com>
2020-01-19 15:43:22 +0100

    EngineConfigurations now have a method to get their stats as string

--- Assets/Scripts/GearConfigurator/EngineConfiguration.cs
+++ Assets/Scripts/GearConfigurator/EngineConfiguration.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Skills;
 using UnityEngine;
 
@@ -8,9 +9,51 @@ namespace GearConfigurator
     {
         public string engineName;
         public SkillId[] skills;
-        public float engineMinSpeedModifier;
-        public float engineMaxSpeedModifier;
-        public float engineBoostSpeedModifier;
-        public float engineBoostTimeModifier;
+        public int minSpeedModifier;
+        public int maxSpeedModifier;
+        public int boostSpeedModifier;
+        public int boostTimeModifier;
+
+        public string GenerateDescription()
+        {
+            var lines = new List<string>
+            {
+                engineName,
+                ""
+            };
+
+            if (skills.Length > 0)
+            {
+                lines.Add("Skills:");
+                foreach (var skillId in skills)
+                {
+                    var skill = SkillDictionary.GetSkill(skillId);
+                    lines.Add(skill.Name);
+                }
+                lines.Add("");
+            }
+
+            if (minSpeedModifier != 0)
+            {
+                lines.Add("Minimum Speed " + minSpeedModifier.ToString("+#:-#;0"));
+            }            
+            
+            if (maxSpeedModifier != 0)
+            {
+                lines.Add("Maximum Speed " + maxSpeedModifier.ToString("+#:-#;0"));
+            }            
+            
+            if (boostSpeedModifier != 0)
+            {
+                lines.Add("Boost Speed " + boostSpeedModifier.ToString("+#:-#;0"));
+            }            
+            
+            if (boostTimeModifier != 0)
+            {
+                lines.Add("Boost Time " + boostTimeModifier.ToString("+#:-#;0"));
+            }            
+            
+            return string.Join("\n", lines);
+        }
     }
 }
